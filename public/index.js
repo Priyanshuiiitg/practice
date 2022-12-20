4418b7ded60f30328059946323d9f9feeffa1261
Priyanshuiiitg <98309398+Priyanshuiiitg@users.noreply.github.com>
2022-12-20 16:00:57 +0530

    Initial commit

---
+++ public/index.js
@@ -0,0 +1,9 @@
+import r from "react";
+import rd from "react-dom";
+rd.render(
+  <div>
+    <h1>Hello World</h1>
+    <p>Hi, My name is Priyanshu Shukla</p>
+  </div>,
+  document.getElementById("root")
+);
